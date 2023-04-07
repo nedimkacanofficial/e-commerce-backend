40058a9d8e6b115bd04e0aa3f968901e054027b5
ndmkcn <ndmkcn1453@gmail.com>
2023-04-08 02:01:48 +0300

    The customer repository has been created.

---
+++ src/main/java/com/ndmkcn/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.ndmkcn.ecommerce.dao;
+
+import com.ndmkcn.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
